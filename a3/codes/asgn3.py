988d8d607065bdc299f6473239bea1063c1f0021
Sarvesh2119 <141245446+Sarvesh2119@users.noreply.github.com>
2023-08-12 22:20:43 +0530

    Add files via upload

---
+++ a3/codes/asgn3.py
@@ -0,0 +1,32 @@
+import numpy as np
+A=np.array([1,-1])
+B=np.array([-4,6])
+C=np.array([-3,-5])
+omat = np.array([[0,1],[-1,0]]) 
+
+#direction vector
+def dir_vec(A,B):
+  return B-A
+  
+#normal vector 
+def norm_vec(A,B):
+  return omat@dir_vec(A,B)
+
+#normal vectors to BE and CF 
+n_BE=dir_vec(C,A)
+n_CF=dir_vec(B,A)
+
+#Intersection of two lines 
+def line_intersect(n1,A1,n2,A2):
+  N=np.block([[n1],[n2]])
+  p = np.zeros(2)
+  p[0] = n1@A1
+  p[1] = n2@A2
+  #Intersection
+  P=np.linalg.solve(N,p)
+  return P
+  
+H=line_intersect(n_BE,B,n_CF,C)
+
+verify=((A-H).T)@(B-C)
+print(verify)
