be2221662d90221cc53c9bfec6c364fab306302f
Sarvesh2119 <141245446+Sarvesh2119@users.noreply.github.com>
2023-10-10 20:00:15 +0530

    Add files via upload

--- gate2/codes/gen.c
+++ gate2/codes/gen.c
@@ -11,17 +11,105 @@ int main() {
     int sample_size = 10000;
 
     // Generate X from an exponential distribution (exp(1))
-    FILE *file_x = fopen("random_x.txt", "w");
-    if (file_x == NULL) {
+    FILE *file_x_w = fopen("random_x.txt", "w");
+    if (file_x_w == NULL) {
         fprintf(stderr, "Error opening file for X\n");
         return 1;
     }
     for (int i = 0; i < sample_size; i++) {
         double x = -log(rand() / (double)RAND_MAX);
-        fprintf(file_x, "%lf\n", x);
+        fprintf(file_x_w, "%lf\n", x);
     }
-    fclose(file_x);
+    fclose(file_x_w);
 
+FILE *file_x = fopen("random_x.txt", "r"); // Open in read mode
+    if (file_x == NULL) {
+        fprintf(stderr, "Error opening file for X\n");
+        return 1;
+    }
+
+    // Generate Y = e^(-X)
+    FILE *file_e_minus_X = fopen("e_minus_X.txt", "w");
+    if (file_e_minus_X == NULL) {
+        fprintf(stderr, "Error opening file for e^(-X)\n");
+        fclose(file_x); // Close the file
+        return 1;
+    }
+
+    // Generate 1 - e^(-X)
+    FILE *file_one_minus_e_minus_X = fopen("one_minus_e_minus_X.txt", "w");
+    if (file_one_minus_e_minus_X == NULL) {
+        fprintf(stderr, "Error opening file for 1 - e^(-X)\n");
+        fclose(file_x); // Close the file
+        fclose(file_e_minus_X); // Close the file
+        return 1;
+    }
+
+    // Generate 2e^(-X) - 1
+    FILE *file_two_e_minus_X_minus_1 = fopen("two_e_minus_X_minus_1.txt", "w");
+    if (file_two_e_minus_X_minus_1 == NULL) {
+        fprintf(stderr, "Error opening file for 2e^(-X) - 1\n");
+        fclose(file_x); // Close the file
+        fclose(file_e_minus_X); // Close the file
+        fclose(file_one_minus_e_minus_X); // Close the file
+        return 1;
+    }
+
+    // Generate a sample of random numbers from a uniform distribution between -1 and 1
+    FILE *file_uniform_minus_1_to_1 = fopen("uniform_minus_1_to_1.txt", "w");
+    if (file_uniform_minus_1_to_1 == NULL) {
+        fprintf(stderr, "Error opening file for Uniform (-1 to 1)\n");
+        fclose(file_x); // Close the file
+        fclose(file_e_minus_X); // Close the file
+        fclose(file_one_minus_e_minus_X); // Close the file
+        fclose(file_two_e_minus_X_minus_1); // Close the file
+        return 1;
+    }
+
+    // Generate a sample of random numbers from a uniform distribution between 0 and 1
+    FILE *file_uniform_0_to_1 = fopen("uniform_0_to_1.txt", "w");
+    if (file_uniform_0_to_1 == NULL) {
+        fprintf(stderr, "Error opening file for Uniform (0 to 1)\n");
+        fclose(file_x); // Close the file
+        fclose(file_e_minus_X); // Close the file
+        fclose(file_one_minus_e_minus_X); // Close the file
+        fclose(file_two_e_minus_X_minus_1); // Close the file
+        fclose(file_uniform_minus_1_to_1); // Close the file
+        return 1;
+    }
+
+    for (int i = 0; i < sample_size; i++) {
+        double x;
+        fscanf(file_x, "%lf", &x);
+        
+        // Calculate Y = e^(-X) and write to file
+        double y = exp(-x);
+        fprintf(file_e_minus_X, "%lf\n", y);
+
+        // Calculate 1 - e^(-X) and write to file
+        double one_minus_e_minus_x = 1 - exp(-x);
+        fprintf(file_one_minus_e_minus_X, "%lf\n", one_minus_e_minus_x);
+
+        // Calculate 2e^(-X) - 1 and write to file
+        double two_e_minus_x_minus_1 = 2 * exp(-x) - 1;
+        fprintf(file_two_e_minus_X_minus_1, "%lf\n", two_e_minus_x_minus_1);
+
+        // Generate a sample of random numbers from a uniform distribution between -1 and 1 and write to file
+        double u_minus_1_to_1 = -1.0 + 2.0 * ((double)rand() / (double)RAND_MAX);
+        fprintf(file_uniform_minus_1_to_1, "%lf\n", u_minus_1_to_1);
+
+        // Generate a sample of random numbers from a uniform distribution between 0 and 1 and write to file
+        double u_0_to_1 = (double)rand() / (double)RAND_MAX;
+        fprintf(file_uniform_0_to_1, "%lf\n", u_0_to_1);
+    }
+
+    // Close all files
+    fclose(file_x);
+    fclose(file_e_minus_X);
+    fclose(file_one_minus_e_minus_X);
+    fclose(file_two_e_minus_X_minus_1);
+    fclose(file_uniform_minus_1_to_1);
+    fclose(file_uniform_0_to_1);
     return 0;
 }
 
--- gate2/codes/sim.py
+++ gate2/codes/sim.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stats
-
 # Set the rate parameter for the exponential distribution to 1
 lambda_param = 1
 
@@ -9,20 +7,20 @@ lambda_param = 1
 sample_size = 10000
 exponential_samples = np.loadtxt("random_x.txt")
 
-# Calculate e^(-X) for each sample
-e_minus_X = np.exp(-exponential_samples)
-
-# Generate a sample of random numbers from a uniform distribution between 0 and 1
-uniform_samples_0_to_1 = np.random.uniform(0, 1, size=sample_size)
-
 # Calculate 1 - e^(-X)
-one_minus_e_minus_X = 1 - e_minus_X
+one_minus_e_minus_X = np.loadtxt("one_minus_e_minus_X.txt")
+
+# Calculate e^(-X)
+e_minus_X = np.loadtxt("e_minus_X.txt")
 
 # Generate a sample of random numbers from a uniform distribution between -1 and 1
-uniform_samples_minus_1_to_1 = np.random.uniform(-1, 1, size=sample_size)
+uniform_samples_minus_1_to_1 = np.loadtxt("uniform_minus_1_to_1.txt")
+
+# Generate a sample of random numbers from a uniform distribution between 0 and 1
+uniform_samples_0_to_1 = np.loadtxt("uniform_0_to_1.txt")
 
 # Calculate 2e^(-X) - 1
-two_e_minus_X_minus_1 = 2 * e_minus_X - 1
+two_e_minus_X_minus_1 = np.loadtxt("two_e_minus_X_minus_1.txt")
 
 # Calculate the CDFs
 ecdf_e_minus_X = np.arange(1, len(e_minus_X) + 1) / len(e_minus_X)
@@ -30,9 +28,6 @@ ecdf_uniform_0_to_1 = np.arange(1, len(uniform_samples_0_to_1) + 1) / len(unifor
 ecdf_one_minus_e_minus_X = np.arange(1, len(one_minus_e_minus_X) + 1) / len(one_minus_e_minus_X)
 ecdf_uniform_minus_1_to_1 = np.arange(1, len(uniform_samples_minus_1_to_1) + 1) / len(uniform_samples_minus_1_to_1)
 ecdf_two_e_minus_X_minus_1 = np.arange(1, len(two_e_minus_X_minus_1) + 1) / len(two_e_minus_X_minus_1)
-
-# Create subplots for three comparisons with larger plot size
-fig, axs = plt.subplots(1, 3, figsize=(20, 10))
 
 plt.figure(figsize=(10, 10))
 plt.plot(np.sort(e_minus_X), ecdf_e_minus_X, color='blue', label='CDF of e^(-X)')
@@ -43,7 +38,7 @@ plt.legend()
 plt.grid(True)
 plt.savefig("cdf_comp_1.png")
 
-#plt.figure(figsize=(10, 10))
+plt.figure(figsize=(10, 10))
 plt.plot(np.sort(one_minus_e_minus_X), ecdf_one_minus_e_minus_X, color='blue', label='CDF of 1 - e^(-X)')
 plt.plot(np.sort(uniform_samples_0_to_1), ecdf_uniform_0_to_1, color='red', linestyle='--', label='CDF of Uniform (0 to 1)')
 plt.xlabel('Value')
@@ -62,5 +57,3 @@ plt.grid(True)
 plt.savefig("cdf_comp_3.png")
 
 plt.tight_layout()
-
-
